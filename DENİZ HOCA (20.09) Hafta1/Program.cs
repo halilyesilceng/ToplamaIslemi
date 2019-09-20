d27686efa8d5826b91bb6f938564a277a0740ee0
Halil YEŞİL <halil.yesil@bakircay.edu.tr>
2019-09-20 14:12:42 +0300

    Add project files.

---
+++ DENİZ HOCA (20.09) Hafta1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DENİZ_HOCA__20._09__Hafta1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
